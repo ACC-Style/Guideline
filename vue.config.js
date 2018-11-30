c2c67fefeb1a0008ffc42dfce328437d32caee15
Matt Watier <mattwatier@gmail.com>
2018-11-30 11:14:53 -0500

    Git Build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  baseUrl: '/Guildeline',
-  outputDir: 'docs'
+  baseUrl: '/Guideline',
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
